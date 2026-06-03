f9dd3eeb0f6d05ea78a04b7c41aa5a464ed10afa
Daniel Jose Da Silva <42511939+danielsilvajobs@users.noreply.github.com>
2026-06-03 15:40:19 +0100

    Update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://k8sm8s.com',
   integrations: [react(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       theme: 'dark-plus',
